2447cd56b093e5aa5b59bbda1368a21850fbc754
sdear <sdear@blackcreekisc.com>
2025-10-13 13:39:09 -0500

    progress3

--- flags/src/app/app.component.ts
+++ flags/src/app/app.component.ts
@@ -18,6 +18,10 @@ export class AppComponent {
   currentIndex = 0;
   guessCountries: Country[] = [];
   showAutoComplete = false;
+  currentName = '';
+  message = '';
+  guessCounter = 0; // wrong guess counter
+  gameState = 'start'; //kinda like state management.
 
 
   constructor() {
@@ -36,8 +40,12 @@ export class AppComponent {
     //1 this.currentCountry = this.countries[uniqueIndexes[0]];
 
     // 5
+    // 6 somehow this died (no flags) -> refactored.
     for (const i of countryIndexes) {
-      this.guessCountries.push(COUNTRIES[i]);
+      this.guessCountries.push({
+        ...COUNTRIES[i],
+        code: COUNTRIES[i].code.toLowerCase(),
+    });
     }
   }
   
@@ -50,25 +58,70 @@ export class AppComponent {
     // console.log(`selected country index value ${i})`); // log index for now.
   // }
 
-  //8 selectCountry - by country code 
+  //8 selectCountry - by country code
+    
   selectCountry(code: string) {
+  //11 whenever there is a selectCountry event(incident)
     console.log(code);
+    this.currentName = this.countries.filter(c => c.code === code)[0].name
+    this.showAutoComplete = false;  // no Idea what this does in the UI 45:04
     // console.log(`hit selectCountry`);
     // console.log(`selected country index value ${i})`); // log index for now.
   }
 
-  //6 keyup
+  //6 keyup 10 when there is a value
   keyUp(keyUp: any) {
     console.log('Hit Keyup');
     this.showAutoComplete = true;
     console.log(keyUp);
     
-    const val = keyUp.target.value;
-    console.log("Val:", keyUp.target.value); // key in "z" to the input field and console logs out val as z.  
-    //9 "includes" the value. Text getting predictive now. Mon --> Mongolia.  
+    // const val = keyUp.target.value;
+    this.currentName = keyUp.target.value;
+    //console.log("Val:", keyUp.target.value); // key in "z" to the input field and console logs out val as z.  
+    //9 "includes" the value. Text getting predictive now. Mon --> Mongolia.
+    // dep. c.name.toLowerCase().includes(val.toLowerCase())  
     this.countries = COUNTRIES.filter((c) => 
-      c.name.toLowerCase().includes(val.toLowerCase())
+      c.name.toLowerCase().includes(this.currentName.toLowerCase())
      );
+
+     //13 if the countries list is zero just hide the dropdown 
+     if (this.countries.length === 0){
+        this.showAutoComplete = false;
+     } 
+  }
+
+   guess() {
+    this.message = ''; //12 reset message to empty 
+    const guessedCountry = COUNTRIES.find(c => c.name === this.currentName);
+    if (!guessedCountry) {
+      this.message = "You have to select from the listed countries";
+      return;
+    }
+    console.log('Cheater!! Current Flag', this.guessCountries[this.currentIndex].name);
+    console.log('the guessed country', guessedCountry);
+
+    //15 if its wrong guess again 
+    if (guessedCountry.code.toLocaleLowerCase() !== this.guessCountries[this.currentIndex].code) {
+          this.message = "Guess again!";
+          this.guessCounter++;
+          return;
+    }
+
+    this.currentIndex++; //increase the current index (why?)
+    
+    //16
+    this.countries =  COUNTRIES.map(c => ({...c, code: c.code.toLocaleLowerCase(),}));
+    
+    this.currentName = "";
+
+
+    // equal to SIZE, our limit. the game is finished. 
+    if (this.currentIndex === SIZE) {
+        this.gameState === "finished"; //kinda like state management.
+    }
+
+    //14 currentIndex is the current country we are guessing. we guessed correctly. He deleted this. 
+    // if (guessedCountry.code === this.guessCountries[this.currentIndex].code) {}
   }
 
   //Pick a random country. Of type Country. Return a Country.  Many countries. 300. Wowza.
